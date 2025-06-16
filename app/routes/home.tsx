eda86f3bde449e6a6153572bdf2a7fb98d2c8fd7
MohammadAzeem Wani <mohammadazeemwani@mrma.local>
2025-06-16 20:56:33 +0530

    # Project setup from groud up again

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ML Pipelines" },
+    { name: "description", content: "Make Pipelines for your next big AI thing" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <></>;
 }
