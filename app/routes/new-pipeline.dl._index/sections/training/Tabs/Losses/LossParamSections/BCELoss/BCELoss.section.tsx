1737ff5e30381d1e2b3374179db88cdaf88b45d7
MohammadAzeem <mohammadazeemwani@gmail.com>
2025-07-27 12:07:39 +0530

    change made for ubuntu build fix

--- app/routes/new-pipeline.dl._index/sections/training/Tabs/Losses/LossParamSections/BCELoss/BCELoss.section.tsx
+++ app/routes/new-pipeline.dl._index/sections/training/Tabs/Losses/LossParamSections/BCELoss/BCELoss.section.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { cn } from "~/utils/general"
 import type { LossSectionProps } from "../../loss-param-section.mapper"
-import { WeightField } from "./Weight.field"
 import { SizeAverageField } from "./SizeAverage.field"
+import { WeightField } from "./Weight.field"
 import { ReduceField } from "./Reduce.field"
 import { ReductionField } from "./Reduction.field"
 
